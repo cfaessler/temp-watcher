1e1b87a3c2c7d903fa46fa6739dd0e29e9cf8409
Christian Fässler <faessler.chrigi@gmail.com>
2015-11-12 18:32:23 +0100

    Added init script

--- tools/query.py
+++ tools/query.py
@@ -1,5 +1,6 @@
 from w1thermsensor import W1ThermSensor
 import requests
+import ConfigParser
 from requests.exceptions import ConnectionError
 import time
 import logging
@@ -11,14 +12,19 @@ LOGFILE = 'logging.log'
 logging.basicConfig(filename=LOGFILE, level=LOGGING_LEVEL)
 logger = logging.getLogger('TemperatureSensorReading')
 
+# Read settings
+settings = ConfigParser.RawConfigParser()
+settings.read('../settings.cfg')
+
 sensor = W1ThermSensor()
-interval_secs = 1
+INTERVAL_SECS = settings.getint('Global', 'INTERVAL')
+WEB_SERVER = settings.get('Global', 'WEB_SERVER')
 
 while True:
     temp = sensor.get_temperature()
     logger.info("Temperature is %i" % temp)
     try:
-        r = requests.get('http://localhost:5000/add?value=' + str(temp))
+        r = requests.get('http://%s/add?value=%s' % (WEB_SERVER, temp))
     except ConnectionError as e:
         logger.error("Could not connect to server")
-    time.sleep(interval_secs)
+    time.sleep(INTERVAL_SECS)
