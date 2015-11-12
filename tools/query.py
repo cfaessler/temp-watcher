0b3512e1aa12bdcf7889893bc364962ed2d14e05
Christian Fässler <faessler.chrigi@gmail.com>
2015-11-12 16:35:45 +0100

    Added logging

--- tools/query.py
+++ tools/query.py
@@ -1,19 +1,24 @@
 from w1thermsensor import W1ThermSensor
 import requests
-from requests.exceptions import ConnectionError 
+from requests.exceptions import ConnectionError
 import time
 import logging
 
+# Config section
+LOGGING_LEVEL = logging.DEBUG
+LOGFILE = 'logging.log'
+
+logging.basicConfig(filename=LOGFILE, level=LOGGING_LEVEL)
+logger = logging.getLogger('TemperatureSensorReading')
+
 sensor = W1ThermSensor()
+interval_secs = 1
 
 while True:
-	
-	temp=sensor.get_temperature()
-	logging.info("Temperature is %i" % temp)	
-	try:	
-		r = requests.get('http://localhost:5000/add?value='+str(temp))
-	except ConnectionError as e:
-		logging.error("Could not connect to server")
-	time.sleep(1)
-	
-	
+    temp = sensor.get_temperature()
+    logger.info("Temperature is %i" % temp)
+    try:
+        r = requests.get('http://localhost:5000/add?value=' + str(temp))
+    except ConnectionError as e:
+        logger.error("Could not connect to server")
+    time.sleep(interval_secs)
