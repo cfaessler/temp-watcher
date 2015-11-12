69f4e9fe613e2a24cb53d756eb4e3b377274920f
Christian Fässler <faessler.chrigi@gmail.com>
2015-11-12 17:27:45 +0100

    Added pushover notification

--- web/server.py
+++ web/server.py
@@ -1,15 +1,27 @@
 #!/bin/python
+# -*- coding: utf-8 -*-
 
 import datetime
+import ConfigParser
+import requests
+from requests.exceptions import ConnectionError
 from bson.json_util import dumps
 from pymongo import MongoClient
-from flask import Flask, render_template, request, jsonify, Response
-#from flask.json import dumps
+from flask import Flask, render_template, request, Response
 
 MONGODB_URL = 'mongodb://localhost:27017/'
 DB = 'db'
 
 app = Flask(__name__)
+notified = False
+
+# Read settings
+settings = ConfigParser.RawConfigParser()
+settings.read('settings.cfg')
+THRESHOLD = settings.getint('Global', 'THRESHOLD_DEGREES')
+HYSTERESIS = settings.getint('Global', 'HYSTERESIS')
+PUSH_API_TOKEN = settings.get('Pushover', 'API_TOKEN')
+PUSH_API_USER = settings.get('Pushover', 'API_USER')
 
 
 def get_db():
@@ -24,7 +36,31 @@ def add_value():
     reading = {"value": value,
                "date": datetime.datetime.utcnow()}
     db.readings.insert_one(reading)
+
+    if value < THRESHOLD and not notified:
+        message = 'Die Temperatur ist %s Grad und hat den Grenzwert überschritten. Bitte Holz nachlegen!' % value
+        url = 'https://api.pushover.net/1/messages.json'
+        data = {'token': PUSH_API_TOKEN,
+                'user': PUSH_API_USER,
+                'message': message}
+        try:
+            requests.post(url, data)
+        except ConnectionError:
+            pass
+        notified = True
+
+    if value > THRESHOLD + HYSTERESIS:
+        notified = False
     return "success"
+
+
+@app.route('/settings')
+def config():
+    return render_template('config.htm',
+                           API_TOKEN=PUSH_API_TOKEN,
+                           API_USER=PUSH_API_USER,
+                           THRESHOLD=THRESHOLD,
+                           HYSTERESIS=HYSTERESIS)
 
 
 @app.route('/index')
@@ -44,14 +80,7 @@ def get_latest():
     time = str(datetime.datetime.utcnow())
     latest = get_db().readings.find().sort([('date', -1)]).limit(1)
     data = dumps(latest)
-
-
-    import random, time
-    number = random.randint(1,20)
-    timestamp = int(time.time())
-
     return Response(data, mimetype="application/json")
-    return jsonify([timestamp, number])
 
 
 if __name__ == "__main__":
