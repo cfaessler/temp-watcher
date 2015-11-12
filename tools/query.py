2a8b10587f7d20b9509a5515db251a9564e821d9
Christian Fässler <cfaessle@hsr.ch>
2015-11-12 15:18:33 +0000

    Added exception handling and logging

--- tools/query.py
+++ tools/query.py
@@ -1,5 +1,19 @@
 from w1thermsensor import W1ThermSensor
+import requests
+from requests.exceptions import ConnectionError 
+import time
+import logging
 
 sensor = W1ThermSensor()
-temp=sensor.get_temperature()
-print temp
+
+while True:
+	
+	temp=sensor.get_temperature()
+	logging.info("Temperature is %i" % temp)	
+	try:	
+		r = requests.get('http://localhost:5000/add?value='+str(temp))
+	except ConnectionError as e:
+		logging.error("Could not connect to server")
+	time.sleep(1)
+	
+	
