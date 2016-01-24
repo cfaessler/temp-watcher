8485f0b9623722103922cee9405a1d62a50fb979
Christian Fässler <cfaessle@hsr.ch>
2016-01-24 18:16:45 +0000

    Added initialstate script

---
+++ tools/initial_state.py
@@ -0,0 +1,18 @@
+import time
+from ISStreamer.Streamer import Streamer
+
+streamer = Streamer(bucket_name="Stream Example", bucket_key="[Place A Unique Bucket Identifier Here]", access_key="[Place Your Access Key Here]")
+
+streamer.log("By Messages", "Stream Starting")
+for num in range(1, 20):
+    time.sleep(0.1)
+    streamer.log("My Numbers", num)
+	if num%2 == 0:
+        streamer.log("My Booleans", "false")
+	else: 
+        streamer.log("My Booleans", "true")
+	if num%3 == 0:
+        streamer.log("My Events", "pop")
+	if num%10 == 0:
+        streamer.log("My Messages", "Stream Half Done")
+streamer.log("My Messages", "Stream Done")
