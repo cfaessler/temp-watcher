bcffc2ce2c712921632b5d8892fa06023e8cd22b
Christian Fässler <faessler.chrigi@gmail.com>
2016-01-24 19:39:27 +0100

    Added initialstate streaming

--- tools/query.py
+++ tools/query.py
@@ -1,3 +1,4 @@
+from ISStreamer.Streamer import Streamer
 from w1thermsensor import W1ThermSensor
 import requests
 import ConfigParser
@@ -19,6 +20,9 @@ settings.read('../settings.cfg')
 sensor = W1ThermSensor()
 INTERVAL_SECS = settings.getint('Global', 'INTERVAL')
 WEB_SERVER = settings.get('Global', 'WEB_SERVER')
+ISS_ACCESS_KEY = settings.get('ISS', 'ACCESS_KEY')
+ISS_BUCKET_NAME = settings.get('ISS', 'BUCKET_NAME')
+ISS_BUCKET_KEY = settings.get('ISS', 'BUCKET_KEY')
 
 while True:
     temp = sensor.get_temperature()
@@ -27,4 +31,13 @@ while True:
         r = requests.get('http://%s/add?value=%s' % (WEB_SERVER, temp), auth=(settings.get('Global','User'), settings.get('Global','Password')))
     except ConnectionError as e:
         logger.error("Could not connect to server")
+
+    logger.info("Streaming to initialstate")
+    try:
+        streamer = Streamer(bucket_name=ISS_BUCKET_NAME, bucket_key=ISS_BUCKET_KEY, access_key=ISS_ACCESS_KEY)
+        streamer.log("Temperatur Wasserspeicher", temp)
+        streamer.close()
+    except Exception as e:
+        logger.exception("Exception occurred while streaming to ISS")
+
     time.sleep(INTERVAL_SECS)
